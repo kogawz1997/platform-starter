3820b320c1b8a20acc0ffc2c4e1a6fce3a6e5f2d
KOGA.EXE <tawepornchok@gmail.com>
2026-07-06 22:03:30 +0700

    feat(p1): wire api app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,1 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AdminAuthModule } from './modules/admin-auth/admin-auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    AdminAuthModule,
+    UsersModule,
+  ],
+})
 export class AppModule {}
