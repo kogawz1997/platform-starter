2b6c55d584196a92dd5d5042a5194abfe1e4d166
KOGA.EXE <tawepornchok@gmail.com>
2026-07-07 02:36:31 +0700

    fix(api): increase top up slip payload limit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,15 @@ import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.use(json({ limit: '2mb' }));
+  app.use(urlencoded({ extended: true, limit: '2mb' }));
 
   app.enableCors({
     origin: [
