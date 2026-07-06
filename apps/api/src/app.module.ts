aabf6ddb6bd53026f9a5a4491858e9b78963d037
KOGA.EXE <tawepornchok@gmail.com>
2026-07-07 01:48:20 +0700

    feat(topups): register top ups module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { SettingsModule } from './modules/settings/settings.module';
 import { UsersModule } from './modules/users/users.module';
 import { WalletModule } from './modules/wallet/wallet.module';
+import { TopUpsModule } from './modules/topups/topups.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { WalletModule } from './modules/wallet/wallet.module';
     UsersModule,
     SettingsModule,
     WalletModule,
+    TopUpsModule,
   ],
 })
 export class AppModule {}
