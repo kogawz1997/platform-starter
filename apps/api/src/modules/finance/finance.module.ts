46fcebc6102d5553817a5cbd4da1e67230f6856c
KOGA.EXE <tawepornchok@gmail.com>
2026-07-07 13:03:08 +0700

    feat(api): mount queue summary in finance module

--- apps/api/src/modules/finance/finance.module.ts
+++ apps/api/src/modules/finance/finance.module.ts
@@ -3,10 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
+import { QueuesController } from '../queues/queues.controller';
+import { QueuesService } from '../queues/queues.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [FinanceController],
-  providers: [FinanceService],
+  controllers: [FinanceController, QueuesController],
+  providers: [FinanceService, QueuesService],
 })
 export class FinanceModule {}
