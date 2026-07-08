130b87bd5f7de417ac7fdb70551709d7a8938ad0
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 00:56:37 +0700

    feat(api): register provider adapter registry

--- apps/api/src/modules/game-platform/game-platform.module.ts
+++ apps/api/src/modules/game-platform/game-platform.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { ProviderAdapterRegistry } from './adapters/provider-adapter.registry';
 import { GamePlatformController } from './game-platform.controller';
 import { GamePlatformService } from './game-platform.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [GamePlatformController],
-  providers: [GamePlatformService],
-  exports: [GamePlatformService],
+  providers: [GamePlatformService, ProviderAdapterRegistry],
+  exports: [GamePlatformService, ProviderAdapterRegistry],
 })
 export class GamePlatformModule {}
