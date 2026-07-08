dc667b6996bc561c95c4fb2a3191dbcd7efa169d
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 00:33:02 +0700

    fix(api): import jwt in game platform module

--- apps/api/src/modules/game-platform/game-platform.module.ts
+++ apps/api/src/modules/game-platform/game-platform.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from '../../database/database.module';
 import { GamePlatformController } from './game-platform.controller';
 import { GamePlatformService } from './game-platform.service';
 
-@Module({ controllers: [GamePlatformController], providers: [GamePlatformService], exports: [GamePlatformService] })
+@Module({
+  imports: [DatabaseModule, JwtModule.register({})],
+  controllers: [GamePlatformController],
+  providers: [GamePlatformService],
+  exports: [GamePlatformService],
+})
 export class GamePlatformModule {}
