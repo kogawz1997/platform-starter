d66065f6f8cbe62fb985090c8307ccac9b7fb3f1
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 21:39:57 +0700

    feat(api): wire admin wallet ledger controller

--- apps/api/src/modules/wallet/wallet.module.ts
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { AdminWalletLedgerController } from './admin-wallet-ledger.controller';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [WalletController],
+  controllers: [WalletController, AdminWalletLedgerController],
   providers: [WalletService],
   exports: [WalletService],
 })
