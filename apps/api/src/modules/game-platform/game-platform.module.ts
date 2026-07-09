abe90973c2ff9abc66bf92cf9b0ab26adc915c9a
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 22:48:29 +0700

    feat(api): wire game transfer action workflow

--- apps/api/src/modules/game-platform/game-platform.module.ts
+++ apps/api/src/modules/game-platform/game-platform.module.ts
@@ -8,14 +8,16 @@ import { GamePlatformController } from './game-platform.controller';
 import { AdminGameMoneyController, MemberGameTransferController, ProviderWebhookController } from './game-platform-money.controller';
 import { GamePlatformMoneyService } from './game-platform-money.service';
 import { GamePlatformService } from './game-platform.service';
+import { GameTransferActionController } from './game-transfer-action.controller';
+import { GameTransferActionService } from './game-transfer-action.service';
 import { MemberGamePlatformController } from './member-game-platform.controller';
 import { ProviderPresetController } from './provider-preset.controller';
 import { ProviderPresetService } from './provider-preset.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [GamePlatformController, MemberGamePlatformController, MemberGameTransferController, AdminGameMoneyController, ProviderWebhookController, ProviderPresetController, AdapterTestController],
-  providers: [GamePlatformService, GamePlatformMoneyService, ProviderPresetService, AdapterTestService, ProviderAdapterRegistry],
-  exports: [GamePlatformService, GamePlatformMoneyService, ProviderPresetService, AdapterTestService, ProviderAdapterRegistry],
+  controllers: [GamePlatformController, MemberGamePlatformController, MemberGameTransferController, AdminGameMoneyController, ProviderWebhookController, ProviderPresetController, AdapterTestController, GameTransferActionController],
+  providers: [GamePlatformService, GamePlatformMoneyService, ProviderPresetService, AdapterTestService, GameTransferActionService, ProviderAdapterRegistry],
+  exports: [GamePlatformService, GamePlatformMoneyService, ProviderPresetService, AdapterTestService, GameTransferActionService, ProviderAdapterRegistry],
 })
 export class GamePlatformModule {}
