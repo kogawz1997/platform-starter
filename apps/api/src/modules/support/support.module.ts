d574f6ce91b8754ba42c79e5e944d5e0832fb1bd
KOGA.EXE <tawepornchok@gmail.com>
2026-07-10 03:30:58 +0700

    fix(api): import jwt module for support guards

--- apps/api/src/modules/support/support.module.ts
+++ apps/api/src/modules/support/support.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, JwtModule.register({})],
   controllers: [SupportController],
   providers: [SupportService],
 })
