8897bc09310846bdcbc682a70e0d34bb0c45ca0d
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 07:48:13 +0700

    feat(api): register game money controllers

--- apps/api/src/modules/game-platform/game-platform.module.ts
+++ apps/api/src/modules/game-platform/game-platform.module.ts
@@ -3,13 +3,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { ProviderAdapterRegistry } from './adapters/provider-adapter.registry';
 import { GamePlatformController } from './game-platform.controller';
+import { AdminGameMoneyController, MemberGameTransferController, ProviderWebhookController } from './game-platform-money.controller';
+import { GamePlatformMoneyService } from './game-platform-money.service';
 import { GamePlatformService } from './game-platform.service';
 import { MemberGamePlatformController } from './member-game-platform.controller';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [GamePlatformController, MemberGamePlatformController],
-  providers: [GamePlatformService, ProviderAdapterRegistry],
-  exports: [GamePlatformService, ProviderAdapterRegistry],
+  controllers: [GamePlatformController, MemberGamePlatformController, MemberGameTransferController, AdminGameMoneyController, ProviderWebhookController],
+  providers: [GamePlatformService, GamePlatformMoneyService, ProviderAdapterRegistry],
+  exports: [GamePlatformService, GamePlatformMoneyService, ProviderAdapterRegistry],
 })
 export class GamePlatformModule {}
