ee2e30c17d4a2cd3bfcd79d31d60e13b81c3e44f
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 22:59:46 +0700

    feat(api): wire wallet ledger detail controller

--- apps/api/src/modules/money-ops/money-ops.module.ts
+++ apps/api/src/modules/money-ops/money-ops.module.ts
@@ -4,10 +4,11 @@ import { DatabaseModule } from '../../database/database.module';
 import { MoneyOpsController } from './money-ops.controller';
 import { MoneyOpsService } from './money-ops.service';
 import { ProviderSimulatorController } from './provider-simulator.controller';
+import { WalletLedgerDetailController } from './wallet-ledger-detail.controller';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [MoneyOpsController, ProviderSimulatorController],
+  controllers: [MoneyOpsController, ProviderSimulatorController, WalletLedgerDetailController],
   providers: [MoneyOpsService],
 })
 export class MoneyOpsModule {}
