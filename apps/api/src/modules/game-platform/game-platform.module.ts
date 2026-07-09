9de3482388ed67e7fb736d662578e2e1f7b95192
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 07:00:50 +0700

    feat(api): register member game platform controller

--- apps/api/src/modules/game-platform/game-platform.module.ts
+++ apps/api/src/modules/game-platform/game-platform.module.ts
@@ -4,10 +4,11 @@ import { DatabaseModule } from '../../database/database.module';
 import { ProviderAdapterRegistry } from './adapters/provider-adapter.registry';
 import { GamePlatformController } from './game-platform.controller';
 import { GamePlatformService } from './game-platform.service';
+import { MemberGamePlatformController } from './member-game-platform.controller';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [GamePlatformController],
+  controllers: [GamePlatformController, MemberGamePlatformController],
   providers: [GamePlatformService, ProviderAdapterRegistry],
   exports: [GamePlatformService, ProviderAdapterRegistry],
 })
