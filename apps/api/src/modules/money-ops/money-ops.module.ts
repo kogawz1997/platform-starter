242144253caf210c8a78b0562babfcc5123c9d89
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 13:05:56 +0700

    feat(api): register provider simulator controller

--- apps/api/src/modules/money-ops/money-ops.module.ts
+++ apps/api/src/modules/money-ops/money-ops.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { MoneyOpsController } from './money-ops.controller';
 import { MoneyOpsService } from './money-ops.service';
+import { ProviderSimulatorController } from './provider-simulator.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [MoneyOpsController],
+  controllers: [MoneyOpsController, ProviderSimulatorController],
   providers: [MoneyOpsService],
 })
 export class MoneyOpsModule {}
