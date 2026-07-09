aa6d055f8789f7934b7c35c62aecd0f52fab4670
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 13:13:53 +0700

    fix(api): provide JwtService in money ops module

--- apps/api/src/modules/money-ops/money-ops.module.ts
+++ apps/api/src/modules/money-ops/money-ops.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { MoneyOpsController } from './money-ops.controller';
 import { MoneyOpsService } from './money-ops.service';
 import { ProviderSimulatorController } from './provider-simulator.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, JwtModule.register({})],
   controllers: [MoneyOpsController, ProviderSimulatorController],
   providers: [MoneyOpsService],
 })
